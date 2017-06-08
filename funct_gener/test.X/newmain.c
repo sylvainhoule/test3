83b9ae11bfc41cb322f6f35cba25703759bee3a8
sergehould <sergehould@vaniercollege.qc.ca>
2017-06-08 09:44:59 -0400

    committing GenericTypedef.h

---
+++ funct_gener/test.X/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: sh
+ *  
+ * Created on June 8, 2017, 9:35 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
