93faa6c222c9be4d72ec5f7103da0fa8bf2de634
sergehould <sergehould@vaniercollege.qc.ca>
2017-06-12 11:23:35 -0400

    Committing v1.0.2 Add config

--- lecture_test/test1.X/main.c
+++ lecture_test/test1.X/main.c
@@ -6,6 +6,7 @@
  * Author           Date            Version
  * Serge Hould      12 June 2017    1.0.0 First version
  * Serge Hould      12 June 2017    1.0.1 Add blinking
+ * Serge Hould      12 Jube 2017    1.0.2 Add config
  */
 
 #include <stdio.h>
@@ -15,7 +16,7 @@
  * 
  */
 void main(void) {
-
+    // Config();
     while(1){
         // Blink();
     }
