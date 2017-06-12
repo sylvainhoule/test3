5ba480c6210f7b16ba7d59ce628a0f82d5798852
J. Carter <>
2017-06-12 12:25:31 -0400

    Committing v1.0.2	Clean the code by using macros

--- lecture_test/blink.X/main.c
+++ lecture_test/blink.X/main.c
@@ -12,6 +12,7 @@
  *												Set configuration of the system 
  * John Carter      June 10 2017        v1.0.1	Configure a LED and blink it
  *												in the while loop 
+ * John Carter      June 10 2017        v1.0.2	Clean the code by using macros
  *
  *****************************************************************************/
 
@@ -19,17 +20,16 @@
 #include "mcc_generated_files/mcc.h"
 #include "mcc_generated_files/pin_manager.h"
 #include <libpic30.h>
+#define LED         _LATA0
+#define HALF_SEC    8000000
 
-/*
- * 
- */
 void main(void) {
     OSCILLATOR_Initialize();
     SYSTEM_Initialize();
 
     while(1){
-        _LATA0 ^= 1;
-        __delay32(8000000);
+        LED ^= 1;
+        __delay32(HALF_SEC);
     }
 }
 
