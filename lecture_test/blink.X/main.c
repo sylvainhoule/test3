f076b6a1aa87130980a53574fbcf3e830519e69e
J. Robinson <>
2017-06-12 11:54:31 -0400

    Committing v1.0.0 First version. Configure the system.

---
+++ lecture_test/blink.X/main.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * FileName:     main.c 
+ *	 
+ *
+ * Description:    This project intends to implement an blinking LED.
+ *
+ * REVISION HISTORY:
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Author       	Date                Comments on this revision
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Jason Robinson   June 08 2017        v1.0.0	First version. 
+ *												Set configuration of the system 
+ *
+ *****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+void main(void) {
+
+    while(1){
+        
+    }
+}
+
