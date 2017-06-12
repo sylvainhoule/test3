611ce168cb773fd29a658a1029305fc20ae15fa7
sergehould <sergehould@vaniercollege.qc.ca>
2017-06-12 11:16:57 -0400

    Committing v1.1.2-F_GEN1  Counter implemented

---
+++ lecture_test/test1.X/main.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.c
+ *
+ * Created on June 12, 2017, 11:14 AM
+ * 
+ * Author           Date            Version
+ * Serge Hould      12 June 2017    1.0.0 First version
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+void main(void) {
+
+    while(1){
+        
+    }
+}
+
